2a4c7a67ee8b9928b267c68b42d26599e8ca3f05
Meryem Zeynep ALTUNAL <meryemzeyaltun@gmail.com>
2023-06-05 21:00:41 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+import string
+
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+shortened_urls = {}
+
+
+def generate_short_url(length=6):
+    chars = string.ascii_letters + string.digits
+    short_url = "".join(random.choice(chars) for _ in range(length))
+    return short_url
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        long_url = request.form['long_url']
+        short_url = generate_short_url()
+        while short_url in shortened_urls:
+            short_url = generate_short_url()
+
+        shortened_urls[short_url] = long_url
+
+        return f"Shortened URL: {request.url_root}{short_url}"
+    return render_template("index.html")
+
+
+@app.route("/<short_url>")
+def redirect_url(short_url):
+    long_url = shortened_urls.get(short_url)
+    if long_url:
+        return redirect(long_url)
+    else:
+        return "URL not found", 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
